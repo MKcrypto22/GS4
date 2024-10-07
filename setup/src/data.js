c2d574c9cea6f171f0e465383c6785eb87225bc9
Katerina jser <kmalceva148@gmail.com>
2024-10-07 19:51:05 +0300

    first commit

---
+++ setup/src/data.js
@@ -0,0 +1,30 @@
+// data.js
+const questions = [
+  {
+    id: 1,
+    title: "Do I have to allow the use of cookies?",
+    info: "Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.",
+  },
+  {
+    id: 2,
+    title: "How do I change my My Page password?",
+    info: "Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch enamel pin thundercats fashion axe roof party polaroid chartreuse..",
+  },
+  {
+    id: 3,
+    title: "What is BankID?",
+    info: "Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.",
+  },
+  {
+    id: 4,
+    title: "Whose birth number can I use?",
+    info: "You cannot use someone else's birth number (like a Social Security number, national ID number, or any personal identification number) without their explicit permission, as it is considered illegal and unethical.",
+  },
+  {
+    id: 5,
+    title: "When do I recieve a password ordered by letter?",
+    info: "Virtual DOM — це легка копія реального DOM, яка дозволяє React ефективніше оновлювати інтерфейс.",
+  },
+];
+
+export default questions;
